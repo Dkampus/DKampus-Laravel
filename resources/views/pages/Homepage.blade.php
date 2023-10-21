fb9db2ec53bcda427bdbcc90c80822a5a964c66c
gedeastu <gede.astu@gmail.com>
2023-10-21 23:00:42 +0700

    up

---
+++ resources/views/pages/Homepage.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.HomeLayout')
+@section('content')
+    <h1>hello</h1>
+@endsection`
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[UserController::class,'index']);
+Route::get('/promo',[UserController::class,'promo']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
