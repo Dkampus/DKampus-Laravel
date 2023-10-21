1381200924a8ad0be99d9130ba3e9378dd52f6f2
gedeastu <gede.astu@gmail.com>
2023-10-21 23:10:05 +0700

    up

--- resources/views/layouts/HomeLayout.blade.php
+++ resources/views/layouts/HomeLayout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home - Welcome & Enjoy what you need</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
 <div>
