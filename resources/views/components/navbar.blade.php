6effa74f9b78fc95f21346eeb6775380425a59e7
gedeastu <gede.astu@gmail.com>
2023-10-21 23:40:33 +0700

    up

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="flex flex-row items-center justify-between px-5">
+<a href="/">
+<img src="logoDkampus.svg" alt=""></a>
+
+<div class="border-2 w-72 h-10 border-[#F9832A]/40 pl-3 gap-3 rounded-md overflow-hidden flex flex-row justify-between items-center">
+
+ <img src="serach.svg" alt="" class="w-6 h-full">
+ <input type="" class="w-60 h-full outline-none ring-0 border-none text-[#F9832A] placeholder:font-medium placeholder:text-[#F9832A]" placeholder="Cari Menu">
+</div>
+
+<div class="flex flex-row items-center gap-5">
+    <button>
+    <img src="chat.svg" alt="">
+    </button>
+    <button>
+    <img src="menu.svg" alt="">
+    </button>
+</div>
+</nav>
--- resources/views/pages/Homepage.blade.php
+++ resources/views/pages/Homepage.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.HomeLayout')
 @section('content')
-    <h1>hello</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nam perferendis repellat aspernatur labore. Molestias vitae, est assumenda modi accusamus corporis veniam cum error dolorum officia deserunt. Deleniti, reiciendis iste?</p>
+    @include('components.navbar')
 @endsection`
