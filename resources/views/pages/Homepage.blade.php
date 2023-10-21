2c9b4d2b64c24b15a822d821680492307c82ac33
gedeastu <gede.astu@gmail.com>
2023-10-21 23:02:26 +0700

    up

--- resources/views/pages/Homepage.blade.php
+++ resources/views/pages/Homepage.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.HomeLayout')
 @section('content')
     <h1>hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nam perferendis repellat aspernatur labore. Molestias vitae, est assumenda modi accusamus corporis veniam cum error dolorum officia deserunt. Deleniti, reiciendis iste?</p>
 @endsection`
