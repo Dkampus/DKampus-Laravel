116ef3f69d9b3b87f96d5e873d436ee6ad1ac70d
itsrmdhnaz <Ramadhan.abdul.aziz14@gmail.com>
2023-10-23 11:37:06 +0700

    comment Scrollbar

--- resources/views/uhuy.blade.php
+++ resources/views/uhuy.blade.php
@@ -29,7 +29,7 @@
         <div class="swiper-button-next"></div>
 
         <!-- If we need scrollbar -->
-        <div class="swiper-scrollbar"></div>
+        {{-- <div class="swiper-scrollbar"></div> --}}
     </div>
 
 
