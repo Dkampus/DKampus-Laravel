59eb88fb2754c7fac9845908d5aac50a74671193
gedeastu <gede.astu@gmail.com>
2023-10-23 17:13:58 +0700

    up

--- resources/views/components/carousel/slider-category.blade.php
+++ resources/views/components/carousel/slider-category.blade.php
@@ -1,4 +1,4 @@
-<swiper-container id="Swiper" class="h-28 relative my-1 px-4 w-full flex justify-center items-center rounded-lg overflow-hidden" space-between="10"
+<swiper-container id="Swiper" class="h-28 relative px-4 w-full flex justify-center items-center rounded-lg overflow-hidden" space-between="10"
 slides-per-view="auto" direction="horizontal" mousewheel="true">
     {{$slot}}
 </swiper-container>
