d36aa50fcd6bc5abcab8908a9076e498acdc3008
gedeastu <gede.astu@gmail.com>
2023-10-24 12:40:51 +0700

    up

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,130 @@
-// const searchResults = document.getElementById('search-results');
-// const overlayResults = document.getElementById('overlay-results');
-// function show(){
-//     if(searchResults.style.display === 'none' || searchResults.style.display === ''){
-//         searchResults.style.height = '24rem';
-//         searchResults.style.visibility = 'visible';
-//         searchResults.style.opacity = '100';
-//         searchResults.style.boxShadow = "0px 10px 15px -3px rgba(0,0,0,0.1)";
+const barMenu = document.getElementById('bar-menu');
+const overlayMenu = document.getElementById('overlay-menu');
+function showMenu(){
+    if(barMenu.style.display === 'none' || barMenu.style.display === ''){
+        barMenu.style.width = '23rem';
+        barMenu.style.right = '0rem';
+        barMenu.style.height = '100vh';
+        barMenu.style.visibility = 'visible';
+        barMenu.style.opacity = '100';
+        barMenu.style.boxShadow = "0px 10px 15px -3px rgba(0,0,0,0.1)";
 
-//         // style body
-//         document.body.style.overflow = 'hidden';
+        // style body
+        document.body.style.overflow = 'hidden';
 
-//         overlayResults.style.opacity = '100';
-//         overlayResults.style.display = 'block';
-//         overlayResults.style.visibility = 'visible';
-//     }
-// }
+        overlayMenu.style.opacity = '100';
+        overlayMenu.style.display = 'block';
+        overlayMenu.style.visibility = 'visible';
+    }
+}
 
-// function hide(){
-//     searchResults.style.height = '0rem';
-//     searchResults.style.visibility = 'invisible';
-//     searchResults.style.opacity = '0';
-//     searchResults.style.boxShadow = "0px";
+function hideMenu(){
+    barMenu.style.height = '0rem';
+    barMenu.style.visibility = 'invisible';
+    barMenu.style.opacity = '0';
+    barMenu.style.boxShadow = "0px";
+    barMenu.style.right = "-99rem"
 
-//     // style body
-//     document.body.style.overflow = 'auto';
+    // style body
+    document.body.style.overflow = 'auto';
 
-//     overlayResults.style.display = 'none';
-//     overlayResults.style.opacity = '0';
-// }
+    overlayMenu.style.display = 'none';
+    overlayMenu.style.opacity = '0';
+}
 
-// const searchInput = document.getElementById('search-input');
-// const clearInput = document.getElementById('clear-input');
-// clearInput.addEventListener("click",function(){
-//     searchInput.value = "";
-//     clearInput.style.display = 'none';
-// })
-// searchInput.addEventListener("input",function(){
-//     if(searchInput.value.trim() !== ''){
-//         clearInput.style.display = 'flex';
-//         clearInput.style.flexDirection = 'row';
-//         clearInput.style.justifyContent = 'center';
-//         clearInput.style.alignItems = 'center';
-//         clearInput.style.visibility = 'visible';
+const searchResults = document.getElementById('search-results');
+const overlayResults = document.getElementById('overlay-results');
+function showResults(){
+    if(searchResults.style.display === 'none' || searchResults.style.display === ''){
+        searchResults.style.height = '24rem';
+        searchResults.style.visibility = 'visible';
+        searchResults.style.opacity = '100';
+        searchResults.style.boxShadow = "0px 10px 15px -3px rgba(0,0,0,0.1)";
+
+        // style body
+        document.body.style.overflow = 'hidden';
+
+        overlayResults.style.opacity = '100';
+        overlayResults.style.display = 'block';
+        overlayResults.style.visibility = 'visible';
+    }
+}
+
+function hideResults(){
+    searchResults.style.height = '0rem';
+    searchResults.style.visibility = 'invisible';
+    searchResults.style.opacity = '0';
+    searchResults.style.boxShadow = "0px";
+
+    // style body
+    document.body.style.overflow = 'auto';
+
+    overlayResults.style.display = 'none';
+    overlayResults.style.opacity = '0';
+}
+
+const searchInput = document.getElementById('search-input');
+const clearInput = document.getElementById('clear-input');
+clearInput.addEventListener("click",function(){
+    searchInput.value = "";
+    clearInput.style.display = 'none';
+})
+searchInput.addEventListener("input",function(){
+    if(searchInput.value.trim() !== ''){
+        clearInput.style.display = 'flex';
+        clearInput.style.flexDirection = 'row';
+        clearInput.style.justifyContent = 'center';
+        clearInput.style.alignItems = 'center';
+        clearInput.style.visibility = 'visible';
        
-//     }else{
-//         clearInput.style.display = 'none';
-//         clearInput.style.visibility = 'invisible';
+    }else{
+        clearInput.style.display = 'none';
+        clearInput.style.visibility = 'invisible';
        
-//     }
-// })
+    }
+})
+const swiperEl = document.querySelector('swiper-container')
+const params = {
+      injectStyles: [`
+      'path/to/navigation-element.min.css',
+      'path/to/pagination-element.min.css',
+      .swiper-pagination-bullet {
+        width: 8px;
+        height: 8px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 12px;
+        color: #000;
+        opacity: 1;
+        background: rgba(0, 0, 0, 0.2);
+        transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 150ms;
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #fff;
+        background: #F9832A;
+        width: 20px;
+        height: 8px;
+        border-radius: 50px;
+        transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 150ms;
+      }
+
+      .swiper-button-disabled{
+      position: absolute;
+      /* left: -2rem; */
+      display: none;
+      transition: all;
+      }
+      `],
+    }
+    const buttonEl = document.querySelector('button');
+    buttonEl.addEventListener('click', () => {
+    swiperEl.swiper.slideNext();
+    });
+
+Object.assign(swiperEl, params)
+swiperEl.initialize();
--- resources/views/components/header/menu.blade.php
+++ resources/views/components/header/menu.blade.php
@@ -2,6 +2,6 @@
 <img src="menu.svg" alt="" class="w-8">
 </button>
 <div id="bar-menu" class="h-screen invisible bg-white shadow-xl fixed w-0 top-0 right-0 z-[70]">
-<button onclick="hideMenu()" class="absolute right-0">X</button>
+<button onclick="hideMenu()" class="absolute right-8 top-8">X</button>
 </div>
 <div onclick="hideMenu()" id="overlay-menu" class="fixed bg-black/20 invisible transition-all duration-300 opacity-0 w-screen h-screen z-[60] top-0 left-0"></div>
