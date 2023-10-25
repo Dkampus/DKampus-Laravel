315e97df5e1a671920b5180e5c704f3000be739f
gedeastu <gede.astu@gmail.com>
2023-10-25 14:27:33 +0700

    up

--- resources/views/layouts/PromoLayout.blade.php
+++ resources/views/layouts/PromoLayout.blade.php
@@ -32,10 +32,10 @@
     </header>
     <main class="mb-28">
     <nav class="px-5 my-3 flex flex-row items-center gap-4">
-    <a href="/promo" class="transition-all duration-300 font-semibold  {{$NavPromo === 'Semua' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg'}}">Semua</a>
-    <a href="/promo/makanan" class="transition-all duration-300 font-semibold {{$NavPromo === 'Makanan' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg'}}">Makanan</a>
-    <a href="/promo/minuman" class="transition-all duration-300 font-semibold {{$NavPromo === 'Minuman' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg'}}">Minuman</a>
-    <a href="/promo/cemilan" class="transition-all duration-300 font-semibold {{$NavPromo === 'Camilan' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-lg'}}">Cemilan</a>
+    <a href="/promo" class="transition-all duration-300 font-semibold  {{$NavPromo === 'Semua' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl'}}">Semua</a>
+    <a href="/promo/makanan" class="transition-all duration-300 font-semibold {{$NavPromo === 'Makanan' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl'}}">Makanan</a>
+    <a href="/promo/minuman" class="transition-all duration-300 font-semibold {{$NavPromo === 'Minuman' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl'}}">Minuman</a>
+    <a href="/promo/cemilan" class="transition-all duration-300 font-semibold {{$NavPromo === 'Camilan' ? 'text-white bg-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl':'text-[#F9832A] bg-white border-2 border-[#F9832A] h-10 px-3 flex flex-row items-center rounded-xl'}}">Cemilan</a>
     </nav>
     <div>
     @yield('contentPromo')
