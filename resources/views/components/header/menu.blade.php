d53b4793e52d433c293935d2087abd595a432c02
gedeastu <gede.astu@gmail.com>
2023-10-31 15:20:32 +0700

    up

--- resources/views/components/header/menu.blade.php
+++ resources/views/components/header/menu.blade.php
@@ -1,6 +1,6 @@
 @guest
     <a href="/masuk"
-    class="text-white w-20 h-[2.5rem] rounded-xl text-center bg-[#F9832A] flex items-center gap-2 justify-center font-semibold text-base">
+    class="text-white w-20 h-[2.5rem] rounded-lg text-center bg-[#F9832A] flex items-center gap-2 justify-center font-semibold text-base">
     Masuk
     </a>
 @endguest
