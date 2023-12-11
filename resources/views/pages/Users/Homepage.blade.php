817edb3eb8cb99b7da8d538db1d523c87e65825b
gedeastu <gede.astu@gmail.com>
2023-12-11 12:00:44 +0700

    up: margin top di rekomendasi makanan

--- resources/views/pages/Users/Homepage.blade.php
+++ resources/views/pages/Users/Homepage.blade.php
@@ -74,7 +74,7 @@
 
 
         {{-- Rekomendasi Makanan --}}
-        <div class="flex flex-row justify-between items-center mt-10 mb-5">
+        <div class="flex flex-row justify-between items-center mt-7 md:mt-10  md:mb-5">
             <h1 class="font-semibold w-max text-[4.5vw] sm:text-2xl">Rekomendasi Makanan</h1>
             <a href="" class="text-[#F9832A] text-lg font-semibold">Lihat Semua</a>
         </div>
