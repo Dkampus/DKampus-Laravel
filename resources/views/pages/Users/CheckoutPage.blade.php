26d6d69d80201a001a1d888349fb33ece788f316
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-03-04 17:35:42 +0700

    update checkout - alamat

--- resources/views/pages/Users/CheckoutPage.blade.php
+++ resources/views/pages/Users/CheckoutPage.blade.php
@@ -9,7 +9,11 @@
         <h1 class="font-bold text-black text-xl">Pemesanan</h1>
     </header>
     <main class="flex flex-col w-full h-full">
-        {{-- tolong buat header dari nama toko sebelum perulangan item2 pada cart --}}
+        <?php
+            //temporary data alamat
+            $nama_alamat = "Kost";
+            $alamat = "Jl. Sukapura No.20, Sukapura, Kec. Dayeuhkolot Kabupaten Bandung";
+        ?>
         @php $nama_toko = $carts->first()->menu->data_umkm->nama_umkm @endphp
         <div class="bg-orange-500 flex items-center p-3 rounded">
             <svg height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -79,6 +83,18 @@
         </div>
         <div class="flex flex-col w-full h-auto px-1 py-1 bg-gray-200"></div>
         {{--Alamat Pengiriman--}}
-        disini alamat
+        <div class="flex flex-col w-full h-auto px-1 py-2">
+            <div class="flex flex-row items-center w-full h-auto bg-white rounded-md p-4">
+                <p class="font-bold text-black text-l">Diantarkan ke {{$nama_alamat}}</p>
+                <a href="#" class="ml-auto text-[#F9832A]">Ubah</a>
+            </div>
+            <div class="flex flex-row items-center w-full h-auto bg-white rounded-md p-4">
+                <p class="font-normal text-black text-md">{{$alamat}}</p>
+            </div>
+        </div>
+        {{--Button Pesan--}}
+        <a href="#" class="flex items-center justify-center h-12 bg-orange-500 fixed bottom-0 w-full overflow-auto">
+            <p class="font-bold text-white text-lg">Pembayaran</p>
+        </a>
     </main>
 @endsection
