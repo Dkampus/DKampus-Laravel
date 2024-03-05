af091d39bbd35d7590e83a6728ad19def87a2ca9
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-03-05 21:27:41 +0700

    update svg

--- resources/views/pages/Users/ChatRoomPage.blade.php
+++ resources/views/pages/Users/ChatRoomPage.blade.php
@@ -117,15 +117,12 @@
         @endauth
     </main>
     <footer class="fixed bottom-0 left-0 w-full bg-white z-10 shadow-md py-2 px-4">
-        <form action="{{}}" method="POST">
+        <form action="#" method="POST">
             @csrf
             <div class="flex items-center">
                 <input type="text" name="message" placeholder="Type a message" class="flex-grow border rounded-lg p-2 mr-2">
                 <button type="submit" class="bg-[#F9832A] text-white rounded-lg p-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 .5.5H14a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-3a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h8zm-1 2V4a1 1 0 0 0-1-1h-8a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1.5a.5.5 0 0 1 1 0v2a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2a.5.5 0 0 1-1 0z"/>
-                        <path fill-rule="evenodd" d="M13.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H1.5a.5.5 0 0 0 0 1h10.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                    </svg>
+                    <img src="{{ asset('/send.svg') }}" width="24" height="24" class="bi bi-box-arrow-in-right">
                 </button>
             </div>
         </form>
