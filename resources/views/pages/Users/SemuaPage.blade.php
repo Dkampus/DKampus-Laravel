848b43996324b3652590161737d5c46081264560
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-05-15 03:43:30 +0700

    WIP Promo Content, refixing PromoModel, deleted static menu on model.

--- resources/views/pages/Users/SemuaPage.blade.php
+++ resources/views/pages/Users/SemuaPage.blade.php
@@ -21,7 +21,7 @@
         {{-- Header Card --}}
         <div id="headerCard" class="w-full">
             <img src="{{$Item['Discount']}}" alt="" class="absolute">
-            <img src="{{$Item['Img']}}" alt="" class="w-full h-32">
+            <img src="{{Storage::url($Item['Img'])}}" alt="" class="w-full h-32">
         </div>
 
         {{-- Content card --}}
@@ -29,8 +29,8 @@
 
             {{-- Prices --}}
             <div id="prices" class="flex flex-row items-center gap-2.5 mt-1 h-max overflow-x-auto">
-                <h1 class="text-[#F9832A] font-bold text-xl">{{$Item['PriceDiscount']}}</h1>
-                <h1 class="line-through text-[#BCBCBC] text-sm sm:text-base font-semibold">{{$Item['PriceOri']}}</h1>
+                <h1 class="text-[#F9832A] font-bold text-xl">Rp. {{number_format($Item['PriceDiscount'],0,',','.')}}</h1>
+                <h1 class="line-through text-[#BCBCBC] text-sm sm:text-base font-semibold">{{number_format($Item['PriceOri'],0,',','.')}}</h1>
             </div>
 
             {{-- Desc --}}
@@ -44,7 +44,7 @@
         </div>
     </swiper-slide>
    @empty
-       
+
    @endforelse
 </x-promo-slider.carousel>
 
@@ -52,22 +52,22 @@
 <x-promo-slider.semua-promo-carousel>
     @forelse ($PromoTerlarisSlider as $Item)
      <swiper-slide class="border transition-all hidden duration-300 w-40 h-60 rounded-xl overflow-hidden shadow-md mb-2 md:flex md:flex-col">
- 
+
          {{-- Header Card --}}
          <div id="headerCard" class="w-full">
              <img src="{{$Item['Discount']}}" alt="" class="absolute">
              <img src="{{$Item['Img']}}" alt="" class="w-full h-32">
          </div>
- 
+
          {{-- Content card --}}
          <div id="contentCard" class="px-2 py-2 flex flex-col gap-1 justify-center h-[45%]">
- 
+
              {{-- Prices --}}
              <div id="prices" class="flex flex-row items-center gap-2.5 mt-1 h-max overflow-x-auto">
                  <h1 class="text-[#F9832A] font-bold text-xl">{{$Item['PriceDiscount']}}</h1>
                  <h1 class="line-through text-[#BCBCBC] text-sm sm:text-base font-semibold">{{$Item['PriceOri']}}</h1>
              </div>
- 
+
              {{-- Desc --}}
              <div id="desc" class="flex flex-col gap-0.5">
                  <h1 class="font-semibold text-lg text-wrapper-promo-terlaris">{{$Item['Title']}}</h1>
@@ -79,7 +79,7 @@
          </div>
      </swiper-slide>
     @empty
-        
+
     @endforelse
  </x-promo-slider.semua-promo-carousel>
 
@@ -98,22 +98,22 @@
 <x-promo-slider.semua-promo-carousel>
     @forelse ($PromoTerlarisSlider as $Item)
      <swiper-slide class="border transition-all hidden duration-300 w-40 h-60 rounded-xl overflow-hidden shadow-md mb-2 md:flex md:flex-col">
- 
+
          {{-- Header Card --}}
          <div id="headerCard" class="w-full">
              <img src="{{$Item['Discount']}}" alt="" class="absolute">
              <img src="{{$Item['Img']}}" alt="" class="w-full h-32">
          </div>
- 
+
          {{-- Content card --}}
          <div id="contentCard" class="px-2 py-2 flex flex-col gap-1 justify-center h-[45%]">
- 
+
              {{-- Prices --}}
              <div id="prices" class="flex flex-row items-center gap-2.5 mt-1 h-max overflow-x-auto">
                  <h1 class="text-[#F9832A] font-bold text-xl">{{$Item['PriceDiscount']}}</h1>
                  <h1 class="line-through text-[#BCBCBC] text-sm sm:text-base font-semibold">{{$Item['PriceOri']}}</h1>
              </div>
- 
+
              {{-- Desc --}}
              <div id="desc" class="flex flex-col gap-0.5">
                  <h1 class="font-semibold text-lg text-wrapper-promo-terlaris">{{$Item['Title']}}</h1>
@@ -125,7 +125,7 @@
          </div>
      </swiper-slide>
     @empty
-        
+
     @endforelse
  </x-promo-slider.semua-promo-carousel>
 @endsection
