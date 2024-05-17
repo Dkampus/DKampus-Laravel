0394e2f83da210bbc506b5bdcb47171a11bce397
Razzaq44 <razzaq.bowo@gmail.com>
2024-05-17 23:26:07 +0700

    add exception for ChatController

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chat;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,79 +18,39 @@ class ChatController extends Controller
         return view('Users.ChatRoomPage');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
 
     public function listChat()
     {
-        $currentUserId = Auth::user()->id;
-        return view('pages/Users/ChatPage', [
-            'Title' => 'list-chat',
-            'custId' => $currentUserId,
-        ]);
+        try {
+            $currentUserId = Auth::user()->id;
+            return view('pages/Users/ChatPage', [
+                'Title' => 'list-chat',
+                'custId' => $currentUserId,
+            ]);
+        } catch (Exception $e) {
+            return redirect()->back()->with('error2', 'Error');
+        }
     }
 
     public function roomChat(Request $request)
     {
-        $custId = Auth::user()->id;
-        $courId = $request->input('courId');
-        $database = app('firebase.database');
-        $cour_name = $database->getReference('chats/' . $custId . '-' . $courId . '/cour_name')->getValue();
-        $refKey = $database->getReference('chats/' . $custId . '-' . $courId)->getChildKeys();
-        $date = $database->getReference('chats/' . $custId . '-' . $courId . '/' . $refKey[0] . '/msgs/timestamp')->getValue();
-        // dd($date);
-        return view('pages/Users/ChatRoomPage', [
-            'Title' => 'room-chat',
-            'cour_name' => $cour_name,
-            'custId' => $custId,
-            'courId' => $courId,
-            'date' => $date,
-        ]);
-    }
-
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Chat $chat)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Chat $chat)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Chat $chat)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Chat $chat)
-    {
-        //
+        try {
+            $custId = Auth::user()->id;
+            $courId = $request->input('courId');
+            $database = app('firebase.database');
+            $cour_name = $database->getReference('chats/' . $custId . '-' . $courId . '/cour_name')->getValue();
+            $refKey = $database->getReference('chats/' . $custId . '-' . $courId)->getChildKeys();
+            $date = $database->getReference('chats/' . $custId . '-' . $courId . '/' . $refKey[0] . '/msgs/timestamp')->getValue();
+            // dd($date);
+            return view('pages/Users/ChatRoomPage', [
+                'Title' => 'room-chat',
+                'cour_name' => $cour_name,
+                'custId' => $custId,
+                'courId' => $courId,
+                'date' => $date,
+            ]);
+        } catch (Exception $e) {
+            return redirect()->back()->with('error2', 'Error');
+        }
     }
 }
