30cf25d1593c20077b7c85bb4dd3a496171a9aba
Razzaq44 <razzaq.bowo@gmail.com>
2024-05-20 18:15:02 +0700

    fixing status orders cust

--- resources/views/pages/Users/StatusOrder.blade.php
+++ resources/views/pages/Users/StatusOrder.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <header class="sticky top-0 left-0 flex justify-start w-full bg-white z-10 shadow-md py-3 mb-5">
     <div class="flex flex-row items-center h-full">
-        <a href="{{ 'pesanan/status' }}" class="top-5 left-5 flex items-center px-5">
+        <a href="{{ url('pesanan/status') }}" class="top-5 left-5 flex items-center px-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#F9832A" class="bi bi-arrow-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M10.354 1.646a.5.5 0 0 1 0 .708L5.707 7l4.647 4.646a.5.5 0 0 1-.708.708l-5-5a.5.5 0 0 1 0-.708l5-5a.5.5 0 0 1 .708 0z" />
             </svg>
