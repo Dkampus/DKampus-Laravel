3d093835ca6f48003217943867551e748d33ae42
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-05-21 16:54:34 +0700

    update modal transaction

--- resources/views/pages/Admin/transaction.blade.php
+++ resources/views/pages/Admin/transaction.blade.php
@@ -114,7 +114,7 @@
             </div>
         </div>
         {{-- modal detail transaction --}}
-        <div class="fixed z-10 inset-0 overflow-y-auto " aria-labelledby="modal-detail" role="dialog" aria-modal="true" id="modal-edit">
+        <div class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-detail" role="dialog" aria-modal="true" id="modal-edit">
             <div class="flex items end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
@@ -171,12 +171,12 @@
         <div id="payment-proof-modal" class="hidden fixed z-10 inset-0 overflow-y-auto">
             <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-                <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <img id="payment-proof-image" src="" alt="Payment Proof" class="w-full">
+                <div class="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div class="bg-white dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <img id="payment-proof-image" src="" alt="" class="w-full">
                     </div>
-                    <div class="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onclick="document.getElementById('payment-proof-modal').classList.add('hidden');">
+                    <div class="bg-white dark:bg-gray-800 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onclick="document.getElementById('payment-proof-modal').classList.add('hidden')">
                             Close
                         </button>
                     </div>
