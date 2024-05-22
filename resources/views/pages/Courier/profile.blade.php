5b452b1cfd44d307a29d951861e787a948441d8b
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-05-22 13:13:18 +0700

    update profile page courier view

--- resources/views/pages/Courier/profile.blade.php
+++ resources/views/pages/Courier/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.Root')
 @section('content')
     @include('components.header.courierHeader')
-    <div class="bg-[#F0F3F8] flex flex-col items-center justify-center">
+    <div class="bg-[#F0F3F8] flex flex-col items-center justify-center mb-48">
         <div class="w-full h-full space-y-8 bg-white rounded-lg p-8">
             <div class="profile-section text-center">
                 <img class="w-24 h-24 rounded-full mx-auto object-cover" src="{{ asset('logoDkampus.png') }}" alt="Profil Picture">
@@ -13,10 +13,10 @@
             <div class="mt-8">
                 <h3 class="text-lg font-medium text-gray-900 mb-4">Pengaturan Akun</h3>
                 <div class="grid grid-cols-1 gap-4">
-                    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md text-center">
+                    <a href="#" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-bold py-2 px-4 rounded-md text-center" data-modal-show="ubahPasswordModal">
                         Ubah Password
                     </a>
-                    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md text-center">
+                    <a href="#" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-bold py-2 px-4 rounded-md text-center" data-modal-show="ubahNomorTeleponModal">
                         Ubah Nomor Telepon
                     </a>
                     <a href="#" class="bg-gray-300 text-gray-500 py-2 px-4 rounded-md text-center cursor-not-allowed">
@@ -28,13 +28,13 @@
             <div class="mt-8">
                 <h3 class="text-lg font-medium text-gray-900 mb-4">Seputar DKampus</h3>
                 <div class="grid grid-cols-1 gap-4">
-                    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md text-center">
+                    <a href="#" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-bold py-2 px-4 rounded-md text-center">
                         Tentang Kami
                     </a>
-                    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md text-center">
+                    <a href="#" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-bold py-2 px-4 rounded-md text-center">
                         Hubungi Kami
                     </a>
-                    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md text-center">
+                    <a href="#" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-bold py-2 px-4 rounded-md text-center">
                         Syarat dan Ketentuan
                     </a>
                 </div>
@@ -50,83 +50,110 @@
             </div>
         </div>
     </div>
+
     {{-- Modal Ubah Password --}}
-    <div class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-        <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full md:max-w-xl" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                <form action="#" method="POST">
-                    @csrf
-                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <div class="sm:flex sm:items-start">
-                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                                    Ubah Password
-                                </h3>
-                                <div class="mt-2">
-                                    <div class="mb-2">
-                                        <label for="password" class="block text-sm font-medium text-gray-700">Password Lama</label>
-                                        <input type="password" name="password" id="password" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Password Lama">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="new-password" class="block text-sm font-medium text-gray-700">Password Baru</label>
-                                        <input type="password" name="new-password" id="new-password" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Password Baru">
-                                    </div>
-                                    <div class="mb-2">
-                                        <label for="confirm-password" class="block text-sm font-medium text-gray-700">Konfirmasi Password Baru</label>
-                                        <input type="password" name="confirm-password" id="confirm-password" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Konfirmasi Password Baru">
-                                    </div>
-                                </div>
-                            </div>
+    <div id="ubahPasswordModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 bottom-0 z-50 hidden w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
+        <div class="relative w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow">
+                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="ubahPasswordModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="py-6 px-6 lg:px-8">
+                    <h3 class="mb-4 text-xl font-medium text-gray-900">Ubah Password</h3>
+                    <form class="space-y-6" action="#">
+                        {{-- ... (Form untuk ubah password) ... --}}
+                        <div class="flex flex-col space-y-1">
+                            <label for="password" class="text-sm font-medium text-gray-700">Password Lama</label>
+                            <input type="password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500">
                         </div>
-                    </div>
-                </form>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-[#F9832A] text-base font-medium text-white hover:bg-[#F9832A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
-                        Ubah Password
-                    </button>
-                    <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                        Batal
-                    </button>
+                        <div class="flex flex-col space-y-1">
+                            <label for="new-password" class="text-sm font-medium text-gray-700">Password Baru</label>
+                            <input type="password" id="new-password" name="new-password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500">
+                        </div>
+                        <div class="flex flex-col space-y-1">
+                            <label for="confirm-password" class="text-sm font-medium text-gray-700">Konfirmasi Password Baru</label>
+                            <input type="password" id="confirm-password" name="confirm-password" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500">
+                        </div>
+                        <div class="flex flex-row justify-end space-x-4">
+                            <button type="button" class="bg-red-500 text-white font-semibold py-2 px-4 rounded-md" data-modal-hide="ubahPasswordModal">Batal</button>
+                            <button type="submit" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-semibold py-2 px-4 rounded-md">Simpan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    {{--Modal ubah nomor telepon--}}
-    <div class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-        <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full md:max-w-xl" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                <form action="#" method="POST">
-                    @csrf
-                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <div class="sm:flex sm:items-start">
-                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                                    Ubah Nomor Telepon
-                                </h3>
-                                <div class="mt-2">
-                                    <div class="mb-2">
-                                        <label for="no-telp" class="block text-sm font-medium text-gray-700">Nomor Telepon Baru</label>
-                                        <input type="text" name="no-telp" id="no-telp" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Nomor Telepon Baru">
-                                    </div>
-                                </div>
-                            </div>
+
+    {{-- Modal Ubah Nomor Telepon --}}
+    <div id="ubahNomorTeleponModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 bottom-0 z-50 hidden w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
+        <div class="relative w-full max-w-md max-h-full">
+            <div class="relative bg-white rounded-lg shadow">
+                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="ubahNomorTeleponModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div class="py-6 px-6 lg:px-8">
+                    <h3 class="mb-4 text-xl font-medium text-gray-900">Ubah Nomor Telepon</h3>
+                    <form class="space-y-6" action="#">
+                        {{-- ... (Form untuk ubah nomor telepon) ... --}}
+                        <div class="flex flex-col space-y-1">
+                            <label for="no-telp" class="text-sm font-medium text-gray-700">Nomor Telepon Baru</label>
+                            <input type="tel" id="no-telp" name="no-telp" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500">
                         </div>
-                    </div>
-                </form>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-[#F9832A] text-base font-medium text-white hover:bg-[#F9832A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm w-auto sm:text-sm">
-                        Ubah Nomor Telepon
-                    </button>
-                    <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                        Batal
-                    </button>
+                        <div class="flex flex-row justify-end space-x-4">
+                            <button type="button" class="bg-red-500 text-white font-semibold py-2 px-4 rounded-md" data-modal-hide="ubahNomorTeleponModal">Batal</button>
+                            <button type="submit" class="bg-[#F9832A] hover:bg-[#F9832A] text-white font-semibold py-2 px-4 rounded-md">Simpan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
     @include('components.navbar.navbarCourier')
 @endsection
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const modalUbahPassword = document.getElementById('ubahPasswordModal');
+        const modalUbahNomorTelepon = document.getElementById('ubahNomorTeleponModal');
+
+        const modalUbahPasswordShow = Array.from(document.querySelectorAll('[data-modal-show="ubahPasswordModal"]'));
+        const modalUbahNomorTeleponShow = Array.from(document.querySelectorAll('[data-modal-show="ubahNomorTeleponModal"]'));
+
+        const modalUbahPasswordHide = Array.from(document.querySelectorAll('[data-modal-hide="ubahPasswordModal"]'));
+        const modalUbahNomorTeleponHide = Array.from(document.querySelectorAll('[data-modal-hide="ubahNomorTeleponModal"]'));
+
+        modalUbahPasswordShow.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                modalUbahPassword.classList.remove('hidden');
+                modalUbahPassword.classList.add('flex');
+            });
+        });
+
+        modalUbahNomorTeleponShow.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                modalUbahNomorTelepon.classList.remove('hidden');
+                modalUbahNomorTelepon.classList.add('flex');
+            });
+        });
+
+        modalUbahPasswordHide.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                modalUbahPassword.classList.remove('flex');
+                modalUbahPassword.classList.add('hidden');
+            });
+        });
+
+        modalUbahNomorTeleponHide.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                modalUbahNomorTelepon.classList.remove('flex');
+                modalUbahNomorTelepon.classList.add('hidden');
+            });
+        });
+    });
+</script>
