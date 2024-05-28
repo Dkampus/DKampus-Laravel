15adc459e037e26ec412cee2591d497f41166251
Muhammad Real Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-05-28 15:40:51 +0700

    update controller cs

--- app/Http/Controllers/CsController.php
+++ app/Http/Controllers/CsController.php
@@ -2,38 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use GPBMetadata\Google\Api\Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Exception;
 
 class CsController extends Controller
 {
     public function start(){
-        $database = app('firebase.database');
-        $custId = Auth::user()->id;
-        $cust_name = Auth::user()->nama_user;
-        $msg = request()->input('pertanyaan');
-        date_default_timezone_set('Asia/Jakarta');
-        $timestamp = date('Y-m-d H:i:s');
-        $postData = [
-            'msgs' => [
-                'msg' => $msg,
-                'timestamp' => $timestamp,
-                'role' => "driver",
-            ],
-        ];
-        $database->getReference('chats/' . $custId . '1')->push()->set($postData);
-        $database->getReference('chats/' . $courId . '1' . '/cust_name')->set($cust_name);
-        $database->getReference('chats/' . $courId . '1' . '/cour_name')->set('Admin');
-        $database->getReference('chats/' . $courId . '1' . '/courNewMssg')->set(1);
-        $database->getReference('chats/' . $courId . '1' . '/custNewMssg')->set(0);
+        try {
+            $database = app('firebase.database');
+            $custId = Auth::user()->id;
+            $cust_name = Auth::user()->nama_user;
+            $msg = request()->input('pertanyaan');
+            date_default_timezone_set('Asia/Jakarta');
+            $timestamp = date('Y-m-d H:i:s');
+            $postData = [
+                'msgs' => [
+                    'msg' => $msg,
+                    'timestamp' => $timestamp,
+                    'role' => "driver",
+                ],
+            ];
+            $database->getReference('chats/' . $custId . '1')->push()->set($postData);
+            $database->getReference('chats/' . $custId . '1' . '/cust_name')->set($cust_name);
+            $database->getReference('chats/' . $custId . '1' . '/cour_name')->set('Admin');
+            $database->getReference('chats/' . $custId . '1' . '/courNewMssg')->set(1);
+            $database->getReference('chats/' . $custId . '1' . '/custNewMssg')->set(0);
 
-        return view('pages/Users/ChatRoomPage', [
-            'Title' => 'room-chat',
-            'date' => $timestamp,
-            'custId' => $uid,
-            'courId' => '1',
-            'cour_name' => 'Admin',
-        ]);
+            return view('pages/Users/ChatRoomPage', [
+                'Title' => 'room-chat',
+                'date' => $timestamp,
+                'custId' => $custId,
+                'courId' => '1',
+                'cour_name' => 'Admin',
+            ]);
+        } catch (Exception $e) {
+            dd($e);
+            return redirect()->back()->with('error2', 'Error');
+        }
     }
 }
