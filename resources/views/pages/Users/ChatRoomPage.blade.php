6a52e53081107281c0a9d781cbfd3b2bf421c5c9
Razzaq44 <razzaq.bowo@gmail.com>
2024-06-24 10:16:38 +0700

    fix chatroom and history kurir

--- resources/views/pages/Users/ChatRoomPage.blade.php
+++ resources/views/pages/Users/ChatRoomPage.blade.php
@@ -28,8 +28,8 @@
         @csrf
         <div class="flex items-center mb-2">
             <input id="message-input" type="text" name="message" placeholder="Type a message" class="flex-grow border rounded-lg p-3 mr-2">
-            <input id="image-input" type="file" name="image" accept="image/*" class="hidden">
-            <button type="submit" id="image-btn" class="bg-[#F9832A] rounded-lg p-2 mr-2">
+            <input id="image-input" type="file" name="image" accept="image/*" capture="camera" class="hidden">
+            <button type="button" id="image-btn" class="bg-[#F9832A] rounded-lg p-2 mr-2">
                 <img src="{{ asset('/image-icon.svg') }}" width="24" height="24" class="bi bi-image">
             </button>
             <button type="submit" id="send-btn" class="bg-[#F9832A] rounded-lg p-2">
